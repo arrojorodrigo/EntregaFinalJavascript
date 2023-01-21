5506e1416c825eb2aa05ba74e14fd5038e5b82f8
Rodrigo Medina Arrojo <marrojorodrigo@gmail.com>
2023-01-21 11:29:32 -0300

    main,index, css & json

--- main.js
+++ main.js
@@ -86,4 +86,26 @@ const pintarCarrito = () => {
         fragment.appendChild(clone)
     })
     items.appendChild(fragment)
+
+    pintarFooter()
+}
+
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+        footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `
+        // return
+    }
+
+ ///// Sumar cantidad y totales //////
+    const nCantidad = Object.values(carrito).reduce((acc,{cantidad}) => acc + cantidad,0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio,0)
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
 }
